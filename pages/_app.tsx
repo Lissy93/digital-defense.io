1a31685446834d0a89b901cfd1de856dcfcb9401
Alicia Sykes <alicia@omg.lol>
2021-12-04 17:15:46 +0000

    :lipstick: Uses new style contants within app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { ThemeProvider, theme } from '@chakra-ui/react';
+import { ThemeProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import theme from '@/styles/theme';
+
 import '@/styles/globals.css';
 
 const App = function App({ Component, pageProps }: AppProps) {
